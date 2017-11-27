1324d576e58c3bd1b70f5804ea3574291e63edbc
Taavi Kivisik <taavi.kivisik@gmail.com>
2017-11-27 23:19:56 +0100

    Create paste testing folder with names.txt and ages.txt

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir paste_testing_folder
+
+echo Hannah > paste_testing_folder/names.txt
+echo Joe >> paste_testing_folder/names.txt
+echo Maria >> paste_testing_folder/names.txt
+echo Sebastian >> paste_testing_folder/names.txt
+
+echo 21 > paste_testing_folder/ages.txt
+echo 22 >> paste_testing_folder/ages.txt
+echo 20 >> paste_testing_folder/ages.txt
+echo 23 >> paste_testing_folder/ages.txt
