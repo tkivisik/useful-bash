8dacd0ac408f13c91cb88cb25ea998e7add7ef87
Taavi Kivisik <taavi.kivisik@gmail.com>
2017-11-28 23:12:57 +0100

    Added cut_testing_folder and a test file

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-mkdir paste_testing_folder
+mkdir paste_testing_folder cut_testing_folder
 
+# Files for paste_testing_folder
 echo Hannah > paste_testing_folder/names.txt
 echo Joe >> paste_testing_folder/names.txt
 echo Maria >> paste_testing_folder/names.txt
@@ -10,3 +11,9 @@ echo 21 > paste_testing_folder/ages.txt
 echo 22 >> paste_testing_folder/ages.txt
 echo 20 >> paste_testing_folder/ages.txt
 echo 23 >> paste_testing_folder/ages.txt
+
+# Files for cut_testing_folder
+echo a,1,2,3,4,5,6 > cut_testing_folder/cut_the_third_column.txt
+echo b,4,3,5,0,8,9 > cut_testing_folder/cut_the_third_column.txt
+echo c,8,4,6,4,8,6 > cut_testing_folder/cut_the_third_column.txt
+echo d,4,6,3,5,5,5 > cut_testing_folder/cut_the_third_column.txt
